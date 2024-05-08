6046642b0f4932b3ded9746cb67d696908f2130f
emoboi01 <1036030@hr.nl>
2024-05-08 11:05:47 +0200

    added structure of usersoverview en userdata

---
+++ native-app/communication/userDataController.cs
@@ -0,0 +1,15 @@
+class userDataController {
+    public static void saveUserData(string data) {
+        // Save data to local storage
+    }
+
+    public static string getUserData() {
+        // Get data from local storage
+        return "data";
+    }
+
+    public static void changeDay(int dayInput) {
+        // Change the day based on a drop down menu selection
+    }
+
+}
---
+++ native-app/communication/userOverviewController.cs
@@ -0,0 +1,9 @@
+class userOverviewController {
+    public void getAllUserData() {
+        // Get specific user data 
+    }
+
+    public static void deleteUserData() {
+        // Delete data from local storage
+    }
+}
