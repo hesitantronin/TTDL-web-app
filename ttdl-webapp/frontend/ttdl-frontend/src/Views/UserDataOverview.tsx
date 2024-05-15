fa451a2de8f0c415562a92189ad1d603155bde09
emoboi01 <1036030@hr.nl>
2024-05-15 11:43:02 +0200

    layout genstyle (work in progress)

--- ttdl-webapp/frontend/ttdl-frontend/src/Views/UserDataOverview.tsx
+++ ttdl-webapp/frontend/ttdl-frontend/src/Views/UserDataOverview.tsx
@@ -1,1 +1,28 @@
 import { useEffect, useState } from 'react';
+import generalStyle from './stylesheets/generalStyle.module.css';
+
+// function UserDataOverview(){
+//     return (
+//         <html lang="en">
+//         <head>
+//           <meta charSet="UTF-8" />
+//           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//           <title>Your Website</title>
+//           <link rel="stylesheet" href="styles.css" />
+//         </head>
+//         <body>
+//           <div className="body">
+//             <div className="container">
+//               <h1>Your Title</h1>
+//               <p>Your text goes here.</p>
+//               <input type="text" placeholder="Input box" />
+//               {/* Add more input boxes or text elements as needed */}
+//             </div>
+//           </div>
+//         </body>
+//       </html>
+//     );
+// }
+
+
+// export default UserDataOverview;
