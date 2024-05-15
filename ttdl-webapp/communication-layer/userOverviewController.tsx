ac1a8c0ab7a7ec722d5ae92a447a95e6b88fd50b
emoboi01 <1036030@hr.nl>
2024-05-15 11:08:41 +0200

    added communication layer to react project

--- ttdl-webapp/communication-layer/userOverviewController.tsx
+++
@@ -1,9 +0,0 @@
-class userOverviewController {
-    public void getAllUserData() {
-        // Get specific user data 
-    }
-
-    public static void deleteUserData() {
-        // Delete data from local storage
-    }
-}
