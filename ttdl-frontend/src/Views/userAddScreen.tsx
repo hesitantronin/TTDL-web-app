9f6384cd0d70230ca5a64be9dcd9bfe914978d57
emoboi01 <1036030@hr.nl>
2024-05-29 11:35:31 +0200

    Adduser connected to backend

--- ttdl-frontend/src/Views/userAddScreen.tsx
+++ ttdl-frontend/src/Views/userAddScreen.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import generalStyle from './stylesheets/generalStyle.module.css';
-import { Link } from 'react-router-dom';
+import { userAddController } from '../communication/userAddController';
 import Navbar from './navbar';
 
 function AddUserScreen() {
-    const addUser = () => {
-        // Add user logic
+    const { addUser } = userAddController();
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleAddUser = async () => {
+        const user = {
+            Uname: firstName,
+            Password: password,
+            
+        };
+
+        try {
+            await addUser(user);
+            setMessage('Gebruiker succesvol toegevoegd');
+        } catch (error: any) {
+            setMessage('Failed to add user: ' + error.message);
+        }
     };
 
     return (
@@ -19,31 +36,41 @@ function AddUserScreen() {
                         placeholder='Voornaam'
                         name='FirstName'
                         className={generalStyle.inputField2}
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
                     />
                     <input
                         type='text'
                         placeholder='Achternaam'
                         name='LastName'
                         className={generalStyle.inputField2}
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
                     />
+                    {/*  if in the future there is an email server that informs the user an account has been made
                     <input
                         type='email'
                         placeholder='Email'
                         name='Email'
                         className={generalStyle.inputField2}
-                    />
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    /> */}
                     <input
                         type='password'
                         placeholder='Wachtwoord'
                         name='Password'
                         className={generalStyle.inputField2}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                     />
                     <button
-                        onClick={addUser}
+                        onClick={handleAddUser}
                         className={generalStyle.verticalButton}
                     >
                         Gebruiker Toevoegen
                     </button>
+                    {message && <p className={generalStyle.message}>{message}</p>}
                 </div>
             </div>
         </div>
@@ -51,4 +78,3 @@ function AddUserScreen() {
 }
 
 export default AddUserScreen;
-
--- ttdl-frontend/src/communication/userAddController.tsx
+++ ttdl-frontend/src/communication/userAddController.tsx
@@ -2,31 +2,29 @@ import { Console } from 'console';
 import { useEffect, useState } from 'react';
 
 export function userAddController() {
-    //this has to work with the database to add a user
-    // src/controllers/userController.js
     const addUser = async (user: any) => {
-    // try {
-    //     const response = await fetch('http://localhost:28080/api/users/register', {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //         },
-    //         body: JSON.stringify({
-    //             user: user.Uname,
-    //             password: user.Password
-    //         }),
-    //     });
+    try {
+        const response = await fetch('http://localhost:28080/api/users/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ 
+                uname: user.Uname, 
+                password: user.Password
+            }),
+        });
 
-    //     if (!response.ok) {
-    //         throw new Error('Network response was not ok');
-    //     }
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
 
-    //     const result = await response.json();
-    //     return result;
-    // } catch (error) {
-    //     console.error('There was a problem with the fetch operation:', error);
-    //     throw error;
-    // }
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+        throw error;
+    }
 };
 
     return { addUser };
